17049832f23cbf6b80eb95002adb7d81d1806e14
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-06-26 21:07:29 +0200

    gaming clips, angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserModule } from './user/user.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent {
+  @Input() modalID:string = '';
+
+  constructor(public modal:ModalService){}
+
+  closeModal(){
+    this.modal.toggleModal(this.modalID )
+  }
+}
