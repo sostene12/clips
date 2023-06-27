91df6029944e6e0108e9023cc4422c0d1bf790cf
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-06-27 16:39:21 +0200

    ft(clips): user part
    - implemented components designs
    - impleme tabs on the popups of register and login
    
    [WIP clips]

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,ElementRef, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,12 +6,18 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() modalID:string = '';
 
-  constructor(public modal:ModalService){}
+  constructor(public modal:ModalService,public el:ElementRef){
+    console.log(el  )
+  }
 
   closeModal(){
     this.modal.toggleModal(this.modalID )
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 }
