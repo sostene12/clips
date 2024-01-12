5601ec68d48327ac40442b338b9a5df269a88477
sostene12 <ngarukiyimanasostene@gmail.com>
2024-01-12 17:16:00 +0200

    ft(file-upload): file uplaod feature
    - implement redirecting the user after uploading
    - implement destroying instance of uploading if the user goes to another page while deploying
    
    [WIP ft-file-uploads ]

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage,AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
 import { last, switchMap } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
@@ -22,6 +23,7 @@ export class UploadComponent {
   inSubmission = false;
   percentage = 0;
   showPercentage = false;
+  task?:AngularFireUploadTask;
 
   user: firebase.User | null = null;
 
@@ -37,16 +39,24 @@ export class UploadComponent {
   constructor(
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
-    private clipsService: ClipService
+    private clipsService: ClipService,
+    private router: Router
   ) {
     auth.user.subscribe((user) => {
       this.user = user;
     });
   }
 
+  ngOnDestroy(): void {
+    this.task?.cancel()
+      
+  }
+
   storeFile($event: Event) {
     this.isDragover = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file = ($event as DragEvent).dataTransfer
+      ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : ($event.target as HTMLInputElement).files?.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
@@ -66,21 +76,21 @@ export class UploadComponent {
 
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
     const clipRef = this.storage.ref(clipPath);
 
-    task.percentageChanges().subscribe((progress) => {
+    this.task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
 
-    task
+    this.task
       .snapshotChanges()
       .pipe(
         last(),
         switchMap(() => clipRef.getDownloadURL())
       )
       .subscribe({
-        next: (url) => {
+        next: async (url) => {
           const clip = {
             uid: this.user?.uid,
             displayName: this.user?.displayName,
@@ -88,14 +98,17 @@ export class UploadComponent {
             fileName: `${clipFileName}.mp4`,
             url,
           };
-
-          this.clipsService.createClip(clip);
-          console.log(clip);
+          
+          const clipDocRef = await this.clipsService.createClip(clip);
 
           this.alertColor = 'green';
           this.alertMsg =
             'Success! Your clip is now ready to share with the world.';
           this.showPercentage = false;
+
+          setTimeout(() => {
+            this.router.navigate([ 'clip',clipDocRef.id])
+          },1000)
         },
         error: (error) => {
           this.uploadForm.enable();
